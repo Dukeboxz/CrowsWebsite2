23b2a2384f56fa4472a0dea28f10ca53e5781234
Dukeboxz <u04sjj@gmail.com>
2020-06-02 10:33:44 +0100

    init commit

---
+++ CrowsWebsite/Controllers/MemberRegisterController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CrowsWebsite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrowsWebsite.Controllers
+{
+    public class MemberRegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            Member newMember = new Member();
+            newMember.FirstName = "Stephen";
+            return View("Index", newMember);
+        }
+    }
+}
